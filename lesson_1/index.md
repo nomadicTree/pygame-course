030700f9efe5fb8f8c7bdac83f14ad6837bc04f6
Joshua Cussen <cussen.joshua@gmail.com>
2025-11-30 19:37:28 +0000

    start of lesson 1

---
+++ lesson_1/index.md
@@ -0,0 +1,9 @@
+# Predict
+
+## Before running the starter code, make predictions about the program will do.
+
+1. What colour will the background be?
+2. What shape will be drawn? Where?
+3. What will happen to the shape over time?
+4. What do you think `clock.tick(60)` controls?
+5. What will happen when you close the window?
---
+++ lesson_1/starter_code.py
@@ -0,0 +1,34 @@
+import pygame
+import sys
+
+pygame.init()
+
+WIDTH = 600
+HEIGHT = 400
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My First Pygame Window")
+
+clock = pygame.time.Clock()
+
+x = 300  # circle’s starting x-position
+
+running = True
+while running:
+    # --- EVENT HANDLING ---
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # --- UPDATE WORLD ---
+    x = x + 1  # move circle slowly to the right
+
+    # --- DRAW WORLD ---
+    screen.fill((255, 255, 255))
+    pygame.draw.circle(screen, (0, 0, 255), (x, 200), 40)
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
